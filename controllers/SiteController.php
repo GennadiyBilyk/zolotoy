bd15c2e7848e0971aed1bec7e7bede2a346414a1
Gennadiy <gennadiy_bilyk@mail.ru>
2016-08-06 17:09:06 +0300

    Добавил модуль админ

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -127,18 +127,18 @@ class SiteController extends Controller
 //    /**
 //     * @inheritdoc
 //     */
-//    public function actions()
-//    {
-//        return [
-//            'error' => [
-//                'class' => 'yii\web\ErrorAction',
-//            ],
-//            'captcha' => [
-//                'class' => 'yii\captcha\CaptchaAction',
-//                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-//            ],
-//        ];
-//    }
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
 
 
 //
---
+++ module/admin/AdminModule.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\module\admin;
+
+use Yii;
+use yii\web\HttpException;
+
+/**
+ * admin module definition class
+ */
+class AdminModule extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\module\admin\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+
+        if (Yii::$app->user->isGuest) {
+            return Yii::$app->response->redirect('site/login');
+        }
+
+        if (Yii::$app->user->identity->role_id != 1) {
+            throw new \yii\web\HttpException(403, 'Доступ запрещён.');
+        }
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
